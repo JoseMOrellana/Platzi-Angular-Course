2bef1beb5ebcec84703b13fe359fed35cfc1aef1
Jose Orellana <jose.mom.1304@gmail.com>
2018-10-02 19:53:48 -0430

    Learning about directives & data binding. Proyect already has bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzisquare';
+  places:any = [
+  	{plan: "pagado",	nearness: 1, distance: 1, 	active: true,name:"Floreria La Gardenia"},
+  	{plan: "gratuito",	nearness: 1, distance: 1.8,	active: true,name:"Donas La Pasadita"},
+  	{plan: "gratuito",	nearness: 2, distance: 5,	active: true,name:"Veterinaria Huellas Felices"},
+  	{plan: "gratuito",	nearness: 3, distance: 10,	active: false,name:"Sushi Roll"},
+  	{plan: "pagado",	nearness: 3, distance: 35,	active: true,name:"Hotel La Gracia"},
+  	{plan: "gratuito",	nearness: 3, distance: 120,	active: false,name:"Zapateria El Clavo"},
+  ]
+
+  lat:number = 10.2357194;
+  lng:number = -67.965496;
 }
